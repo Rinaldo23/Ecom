1941edb4e4e1513a0612dc36b54ab40556644599
Rinaldo <rinaldobadigar98@gmail.com>
2023-01-16 20:07:55 +0530

    Footer Implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Home from './pages/Home/Home'
-import Products from './pages/Products/Products'
-import Product from './pages/Product/Product'
-import './App.scss'
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import "./App.scss";
 
 function App() {
   return (
