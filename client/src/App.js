f22c66af32b1f636760772511c42550dded73dd4
Rinaldo <rinaldobadigar98@gmail.com>
2023-01-16 16:35:38 +0530

    Setting Project

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Products</div>,
+  },
+  {
+    path: "/product/:id",
+    element: <div>Product</div>,
+  },
+]);
+
+function App() {
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
+}
+
+export default App;
