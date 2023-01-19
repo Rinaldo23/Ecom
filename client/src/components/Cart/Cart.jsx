a4a382911b31b30ae71ffa888b6ec5c9745bfbb5
Rinaldo <rinaldobadigar98@gmail.com>
2023-01-19 19:00:11 +0530

    Add to Cart functionality

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,40 +1,28 @@
 import React from "react";
 import "./Cart.scss";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import {useSelector} from "react-redux";
 
 const Cart = () => {
+  const products = useSelector((state) => state.cart.products);
+
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
-      <div className="item">
-        <img
-          src="https://img.freepik.com/free-photo/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.jpg?w=1060&t=st=1673963295~exp=1673963895~hmac=7c046619baa5c3bd1e4162c503062056fd9f970b3829ea9f26fe59ef8daa8e84"
-          alt=""
-        />
-        <div className="details">
-          <h1>Title</h1>
-          <p>
-            Lorem ipsum dolor sit 
-          </p>
-          <div className="price">1 * 200</div>
+      {products.map((item) => (
+        <div className="item">
+          <img
+            src={item.img}
+            alt=""
+          />
+          <div className="details">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit</p>
+            <div className="price">1 * 200</div>
+          </div>
+          <DeleteOutlinedIcon className="delete" />
         </div>
-        <DeleteOutlinedIcon className="delete" />
-      </div>
-
-      <div className="item">
-        <img
-          src="https://img.freepik.com/free-photo/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.jpg?w=1060&t=st=1673963295~exp=1673963895~hmac=7c046619baa5c3bd1e4162c503062056fd9f970b3829ea9f26fe59ef8daa8e84"
-          alt=""
-        />
-        <div className="details">
-          <h1>Title</h1>
-          <p>
-            Lorem ipsum dolor 
-          </p>
-          <div className="price">1 * 200</div>
-        </div>
-        <DeleteOutlinedIcon className="delete" />
-      </div>
+      ))}
 
       <div className="total">
         <span>SUBTOTAL</span>
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -5,70 +5,104 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
 import {useParams} from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
   const id = useParams().id;
-  const images = [
-    "https://img.freepik.com/free-photo/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.jpg?w=1060&t=st=1673963295~exp=1673963895~hmac=7c046619baa5c3bd1e4162c503062056fd9f970b3829ea9f26fe59ef8daa8e84",
-    "https://img.freepik.com/free-photo/close-up-man-shopping-with-laptop_23-2149241375.jpg?w=1060&t=st=1673963337~exp=1673963937~hmac=bc370a441e11a77ee3e8d290a866775414248cbe8f9751849aa0f04403413ba3",
-  ];
-  const [selectedImg, setSelectedImg] = useState(0);
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
+
+  const {data, loading, error} = useFetch(`/products/${id}?populate=*`);
+
+  const dispatch = useDispatch();
+
 
   return (
     <div className="product">
-        <div className="left">
-          <div className="images">
-            <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-            <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+      {error ? "Something went wrong!!" : loading ? (
+        "Loading Data"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
           </div>
-          <div className="mainImg">
-            <img src={images[selectedImg]} alt="" />
-          </div>
-        </div>
-        <div className="right">
-          <h1>Title</h1>
-          <span className="price">$200</span>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-            explicabo et, perspiciatis libero eius obcaecati similique
-            consequuntur aliquam laborum. Dolorum neque tempora reprehenderit
-            consectetur similique fuga illum odio laudantium magni?
-          </p>
-          <div className="quantity">
-            <button
-              onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
-            >
-              -
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">${data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add" onClick={()=> dispatch(addToCart({
+              id: data.id,
+              title:data.attributes.title,
+              desc:data.attributes.desc,
+              price:data.attributes.price,
+              img:data.attributes.img.data.attributes.url,
+              quantity,
+            }))}>
+              <AddShoppingCartIcon /> ADD TO CART
             </button>
-            {quantity}
-            <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-          </div>
-          <button className="add">
-            <AddShoppingCartIcon /> ADD TO CART
-          </button>
-          <div className="links">
-            <div className="item">
-              <FavoriteBorderIcon /> ADD TO WISH LIST
+            <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
             </div>
-            <div className="item">
-              <BalanceIcon /> ADD TO COMPARE
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
             </div>
           </div>
-          <div className="info">
-            <span>Vendor: Polo</span>
-            <span>Product Type: T-Shirt</span>
-            <span>Tag: T-Shirt, Women, Top</span>
-          </div>
-          <hr />
-          <div className="info">
-            <span>DESCRIPTION</span>
-            <hr />
-            <span>ADDITIONAL INFORMATION</span>
-            <hr />
-            <span>FAQ</span>
-          </div>
-        </div>
+        </>
+      )}
     </div>
   );
 };
