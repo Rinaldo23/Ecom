bddceca84aa0c48bfa327716a0130df1b3d2d9bc
Rinaldo <rinaldobadigar98@gmail.com>
2023-01-19 14:09:06 +0530

    Sub-Categories section Implemented

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
-const List = ({products}) => {
+const List = ({products, subCats, catId}) => {
+  const {data, loading, error} = useFetch(
+    `/products?populate=*${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}`
+  );
+
+  console.log(subCats)
+
   return (
     <div className="list">
-      {products?.map((item) => (
+      {data?.map((item) => (
         <Card item={item} key={item.id} />
       ))}
     </div>
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,38 +1,45 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import {useState} from "react";
 import {useParams} from "react-router-dom";
 import List from "../../components/List/List";
+import useFetch from "../../hooks/useFetch";
 import "./Products.scss";
 
 const Products = () => {
   const catId = parseInt(useParams().id);
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
-  const [products, setProducts] = useState([]);
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products?limit=12");
-      const data = await res.json();
-      console.log(data);
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const {data, loading, error} = useFetch(
+    `/sub-categories`
+  );
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
 
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          {products?.map((item) => (
+          {data?.map((item) => (
             <div className="inputItem" key={item.id}>
               <input
                 type="checkbox"
                 id={item.id}
                 value={item.id}
+                onChange={handleChange}
               />
-              <label htmlFor={item.id}>{item.title.substring(0,10)}</label>
+              <label htmlFor={item.id}>{item.attributes.title}</label>
             </div>
           ))}
         </div>
@@ -79,7 +86,7 @@ const Products = () => {
           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt=""
         />
-        <List products={products}/>
+        <List products={data} subCats={selectedSubCats} catId={catId} />
       </div>
     </div>
   );
